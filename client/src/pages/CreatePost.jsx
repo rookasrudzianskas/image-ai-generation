59131336197828d5271aaa32d9fb5f43f1223180
Rokas <rookas.rudzenskas@gmail.com>
2023-01-25 11:00:54 +0200

    Rokas

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { preview } from '../assets';
+import { getRandomPrompt } from '../utils';
+import { FormField, Loader } from '../components';
 
 const CreatePost = ({}) => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  });
+
+  const [generatingImg, setGeneratingImg] = useState(false);
+
+
+  return (
+    <div>
+
+    </div>
+  );
 };
 
 export default CreatePost;
