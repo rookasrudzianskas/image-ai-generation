f38c560ef56d0179d0f408ab3f93cd34d364716d
Rokas <rookas.rudzenskas@gmail.com>
2023-01-25 10:18:50 +0200

    Add a title to the home page
    This is the first step in creating a community showcase.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,10 +3,15 @@ import React, { useEffect, useState } from 'react';
 import { Card, FormField, Loader } from '../components/index.js';
 
 const Home = ({}) => {
+    const [loading, setLoading] = useState(false);
+    const [allPosts, setAllPosts] = useState(null);
+
     return (
-        <div>
-            Home
-        </div>
+        <section className="max-w-7xl mx-auto">
+            <div>
+                <h1 className="font-extrabold text-[#222328] text-[32px]">The Community Showcase</h1>
+            </div>
+        </section>
     );
 };
 
