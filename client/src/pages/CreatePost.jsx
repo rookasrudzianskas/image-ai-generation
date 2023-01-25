89ceb4c1b531b1ed2f8996e632c1b5449252e2c4
Rokas <rookas.rudzenskas@gmail.com>
2023-01-25 11:03:55 +0200

    fix

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -15,12 +15,24 @@ const CreatePost = ({}) => {
   });
 
   const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  }
 
   return (
-    <div>
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">Generate an imaginative image through DALL-E AI and share it with the community</p>
+      </div>
 
-    </div>
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+        </div>
+      </form>
+    </section>
   );
 };
 
