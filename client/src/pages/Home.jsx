0aa958c1c8c5fb1dced4b4ab5a107f427b474273
Rokas <rookas.rudzenskas@gmail.com>
2023-01-25 14:41:10 +0200

    Update API URL
    The API URL has changed from localhost to the Render URL.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = ({}) => {
     (async () => {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://roookas-open-ai-custom-implementation.onrender.com/api/v1/post', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
