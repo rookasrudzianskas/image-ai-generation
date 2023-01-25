c57e5d5ba938ac857adb2e450595e06530325dc2
Rokas <rookas.rudzenskas@gmail.com>
2023-01-25 12:51:11 +0200

    Add OpenAI API key to .env file
    This commit adds the OpenAI API key to the .env file.

--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose';
 
-const connectDB = async (url) => {
+const connectDB = (url) => {
   mongoose.set('strictQuery', true);
-  mongoose.connect(url).then(() => console.log('Connected to MongoDB')).catch((err) => {
-    console.error('failed to connect with mongo');
-    console.error(err);
-  });
-}
+  mongoose.connect(url)
+    .then(() => console.log('connected to mongo'))
+    .catch((err) => {
+      console.error('failed to connect with mongo');
+      console.error(err);
+    });
+};
 
 export default connectDB;
--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -7,7 +7,7 @@ dotenv.config();
 const router = express.Router();
 
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
@@ -19,19 +19,19 @@ router.route('/').get((req, res) => {
 router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
-    console.log("Entered here.")
-    console.log(prompt)
+
     const aiResponse = await openai.createImage({
-      prompt: `${prompt}`,
+      prompt,
       n: 1,
-      size: '1024x1024',
-      response_format: 'b64_json',
+      size: "512x512",
+      // response_format: 'b64_json',
     });
-    console.log("The response>>>>> ", aiResponse.data);
-    const image = aiResponse.data.data[0].b64_json;
-    res.status(200).json({ photo: image });
+    // console.log("DONE")
+    // const image = aiResponse.data.data[0].b64_json;
+    console.log(res.data.data[0].url);
+    res.status(200).json({ photo: 'rokas' });
   } catch (error) {
-    console.error(error);
+    console.error("THis drains me nuts:", error);
     res.status(500).send(error?.response.data.error.message || 'Something went wrong');
   }
 });
