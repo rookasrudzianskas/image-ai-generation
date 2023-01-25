b3fa1dc7c81d31c0d4217fccd3320f982fcfa5f5
Rokas <rookas.rudzenskas@gmail.com>
2023-01-25 14:02:30 +0200

    Add OpenAI API key to the code
    This is a temporary solution to get the code working.
    
    The API key should be stored in a .env file and loaded using dotenv.

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -7,7 +7,7 @@ dotenv.config();
 const router = express.Router();
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: `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`,
 });
 
 const openai = new OpenAIApi(configuration);
