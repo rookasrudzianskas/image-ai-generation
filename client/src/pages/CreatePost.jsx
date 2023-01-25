d6b9edf92c47b514d6717d08be7f6d4d98087f3e
Rokas <rookas.rudzenskas@gmail.com>
2023-01-25 11:34:42 +0200

    feat: add generate image button
    - add generate image button
    - add loading state
    - add loading spinner

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -29,6 +29,10 @@ const CreatePost = ({}) => {
 
   }
 
+  const generateImage = async () => {
+
+  }
+
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -65,7 +69,24 @@ const CreatePost = ({}) => {
               <img src={preview} alt="preview" className="w-9/12 h-9/12 object-contain opacity-40"
               />
             )}
+
+            {generatingImg && (
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                <Loader />
+              </div>
+            )}
           </div>
+        </div>
+
+        <div className="mt-5 flex gap-5">
+          <button type="button" onClick={generateImage} className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center">{generatingImg ? 'Generating...' : 'Generate'}</button>
+        </div>
+
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">** Once you have created the image you want, you can share it with others in the community **</p>
+          <button type="submit" className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+            {loading ? "Sharing..." : "Share with the community"}
+          </button>
         </div>
       </form>
     </section>
